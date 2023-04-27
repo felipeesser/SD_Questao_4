55fd8ce94c8b8cdd0d91f6f1ca895ebe70621d64
Gustavo Miranda <gustavocataldo@id.uff.br>
2023-04-27 10:59:53 -0300

    Atualização do servidor e cliente
    deixei os arquivos mais no padrão que a professora passou no trabalho

--- Server.py
+++ Server.py
@@ -1,6 +1,12 @@
 import rpyc
 
 class MyService(rpyc.Service):
+    def on_connect(self, conn):
+        print("Conexão estabelecida")
+    
+    def on_disconnect(self, conn):
+        print("Conexão encerrada")
+    
     def exposed_soma_vetor(self, vetor):
         # calcula a soma dos elementos do vetor
         soma = sum(vetor)
@@ -10,4 +16,5 @@ class MyService(rpyc.Service):
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
     t = ThreadedServer(MyService, port=18861)
+    print("Servidor aberto...")
     t.start()
