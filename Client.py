9c92b8fcbfd02f06227476c6d22bbacf9a314b87
gustavocataldo <102626753+gustavocataldo@users.noreply.github.com>
2023-04-26 21:25:04 -0300

    resolvendo questoes 567

--- Client.py
+++ Client.py
@@ -1,4 +1,12 @@
+import sys
+import time
 import rpyc
+
+
+if len(sys.argv) < 2:
+    exit("Usage {} SERVER".format(sys.argv[0]))
+    
+server = sys.argv[1]
 
 # pede ao usuário o tamanho do vetor
 n = int(input("Digite o tamanho do vetor: "))
@@ -6,12 +14,15 @@ n = int(input("Digite o tamanho do vetor: "))
 # cria o vetor com elementos variando de 0 a n-1
 vetor = list(range(n))
 # imprime o vetor
-print(vetor)
+# print(vetor)
+start = time.time()
 # conecta ao servidor localmente
-conn = rpyc.connect("localhost", 18861)
-
+conn = rpyc.connect(server, 18861)
+conn._config['sync_request_timeout'] = 100
 # chama o método remoto para calcular a soma do vetor
 soma = conn.root.soma_vetor(vetor)
+end = time.time()
+print(end-start)
 
 # imprime a soma
 print("A soma dos elementos do vetor é:", soma)
--- Server.py
+++ Server.py
@@ -1,9 +1,13 @@
+import time
 import rpyc
 
 class MyService(rpyc.Service):
     def exposed_soma_vetor(self, vetor):
+        start = time.time()
         # calcula a soma dos elementos do vetor
         soma = sum(vetor)
+        end = time.time()
+        print(end-start)
         return soma
 
 # inicia o servidor
